78274b7bb02f273db113dfaaf49c3aae7cef2005
João Vitor matta <77357542+joaomatta@users.noreply.github.com>
2022-06-13 21:53:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const respostas = [
+  "Certeza!",
+  "Não tenho tanta certeza.",
+  "É decididamente assim.",
+  "Não conte com isso.",
+  "Sem dúvidas!",
+  "Pergunte novamente mais tarde.",
+  "Sim, definitivamente!",
+  "Minha resposta é não.",
+  "Você pode contar com isso.",
+  "Melhor não te dizer agora.",
+  "A meu ver, sim.",
+  "Minhas fontes dizem não.",
+  "Provavelmente.",
+  "Não é possível prever agora.",
+  "Perspectiva boa.",
+  "As perspectivas não são tão boas.",
+  "Sim.",
+  "Concentre-se e pergunte novamente.",
+  "Sinais apontam que sim.",  
+]
+const elementoResposta = document.querySelector("#resposta")
+const inputPergunta = document.querySelector("#inputPergunta")
+const buttonPerguntar = document.querySelector("#buttonPerguntar")
+
+function fazerPergunta() {
+   
+    if(inputPergunta.value == ""){
+        alert("Digite a sua pergunta")
+        return
+    }
+
+    buttonPerguntar.setAttribute("disabled", true)
+
+    //Gerar numero aleatorio
+    const totalRespostas = respostas.length
+    const numeroAleatorio = Math.floor(Math.random() * totalRespostas)
+
+    const pergunta = "<div>" + inputPergunta.value + "</div>"
+    elementoResposta.innerHTML = respostas[numeroAleatorio]
+
+    elementoResposta.style.opacity = 1;
+
+    //Limpar a resposta depois de 3 segundos
+    setTimeout(function() {
+        elementoResposta.style.opacity = 0;
+        buttonPerguntar.removeAttribute("disabled")
+    }, 3000)
+
+}
